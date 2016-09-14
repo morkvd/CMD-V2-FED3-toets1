0a1bd80a98c6c1b3d951865a2d154f84b8fa1bff
morkvd <markvdijken@gmail.com>
2016-09-14 23:26:33 +0200

    simplefymap

--- src/chart-compiled.js
+++ src/chart-compiled.js
@@ -1,13 +1,8 @@
 'use strict';
 
 d3.csv('android-app-data.csv', function (data) {
-  var sortedData = data.map(function (_ref) {
-    var AccessCount = _ref.AccessCount;
-    var AppName = _ref.AppName;
-    return {
-      AppName: AppName,
-      AccessCount: Number.parseInt(AccessCount, 10)
-    };
+  var sortedData = data.map(function (obj) {
+    return Object.assign(obj, { AccessCount: Number.parseInt(obj.AccessCount, 10) });
   }).sort(function (a, b) {
     return b.AccessCount - a.AccessCount;
   });
--- src/chart.js
+++ src/chart.js
@@ -1,8 +1,6 @@
 d3.csv('android-app-data.csv', data => {
-  const sortedData = data.map(({ AccessCount, AppName }) => ({
-                            AppName,
-                            AccessCount: Number.parseInt(AccessCount, 10)
-                          }))
-                          .sort((a, b) => b.AccessCount - a.AccessCount);
+  const sortedData = data.map(obj => {
+    return Object.assign(obj, { AccessCount: Number.parseInt(obj.AccessCount, 10) })
+  }).sort((a, b) => b.AccessCount - a.AccessCount);
   console.log(sortedData);
 });
